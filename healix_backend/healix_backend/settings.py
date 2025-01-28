5af36fd75ae6ff44d2434c5a4baefb7f3650ffe3
Timmy <timilehinoluwaseyi07@gmail.com>
2025-01-28 14:49:38 +0100

    added cor_headers

--- healix_backend/healix_backend/settings.py
+++ healix_backend/healix_backend/settings.py
@@ -38,12 +38,14 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "corsheaders",
     "datasets",
     "insights",
     "predictions",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
